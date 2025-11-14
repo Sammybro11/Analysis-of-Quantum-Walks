c978fe201d0d4a749fd50a06b4ba2c03f6e61daa
Samyak <srai890890@gmail.com>
2025-11-14 16:57:56 +0530

    Probability Evolution and Distribution Plotting

---
+++ CTQW/Plotting.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def ProbEvol(times: np.ndarray, region: np.ndarray, labels: list[str] = ["Left", "Between", "Right"]):
+    fig, ax = plt.subplots(figsize = (10, 9))
+
+    for i in range(region.shape[0]):
+        ax.plot(times, region[i], label = labels[i])
+
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Probability")
+    ax.set_title("Region Probabilities vs Time")
+    ax.legend()
+    ax.grid(True)
+    plt.show()
+
+def ProbDist(probability_dist: np.ndarray, t_index: int):
+    nt, N = probability_dist.shape
+
+    fig, ax = plt.subplots(figsize = (10, 9))
+
+    ax.plot(np.arange(N), probability_dist[t_index])
+    ax.set_xlabel("Lattice")
+    ax.set_ylabel("Probability Distribution")
+    ax.grid(True)
+    plt.show()
